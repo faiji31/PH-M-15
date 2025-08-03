00464a13276f29f7d1f0261cd088b603379f081b
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-08-03 12:23:42 +0600

    3rd commit

---
+++ odd.js
@@ -0,0 +1,15 @@
+// let num=61;
+// while(num<=100){
+//     if(num%2!==0){
+//         console.log(num)
+//     }
+    
+//      num+=1;
+// }
+let num=78;
+while(num<=98){
+    if (num%2===0){
+        console.log(num);
+    }
+    num+=1;
+}
