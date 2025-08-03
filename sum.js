9f2a5a51448cf924d7399a06a61e402f56ecbe34
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-08-03 13:26:32 +0600

    4th commit

---
+++ sum.js
@@ -0,0 +1,21 @@
+// let num=81;
+// let sum=0;
+// while (num<=131){
+//     if(num!==0){
+//         sum+=num;
+       
+
+//     }
+//     num+=1;
+// }
+// console.log("Sum of all odd numbers:"+sum);
+// sum of even numbers 206 to 311
+let num=206;
+let sum=0;
+while (num<=311){
+    if (num%2===0){
+        sum+=num;
+    }
+    num+=1;
+}
+console.log("Sum of all even numbers:"+sum);
